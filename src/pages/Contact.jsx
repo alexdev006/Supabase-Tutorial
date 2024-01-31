825bf8f40f95108020dc91f771cc5d3761455190
Alex <alexdev0605@gmail.com>
2024-01-31 21:06:03 +0100

    feat: crud supabase

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,4 @@
+const Contact = () => {
+  return <h2 className="page contact">Contact</h2>;
+};
+export default Contact;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import supabase from "../config/supabaseClient";
+
+//component
+import SmoothieCard from "../components/SmoothieCard";
+
+const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+  const [orderBy, setOrderBy] = useState("created_at");
+
+  const handleDelete = (id) => {
+    setSmoothies((prevSmoothies) => {
+      return prevSmoothies.filter((sm) => sm.id !== id); //false => remove la card utiliser pour refresh le localstate
+    });
+  };
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from("smoothies")
+        .select()
+        .order(orderBy, { ascending: false });
+
+      if (error) {
+        setFetchError("Could not fetch the smoothies");
+        setSmoothies(null);
+        console.log(error);
+      }
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchSmoothies();
+  }, [orderBy]);
+
+  return (
+    <div className="page home">
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          <div className="order-by">
+            <p>order by:</p>
+            <button onClick={() => setOrderBy("created_at")}>
+              Time created
+            </button>
+            <button onClick={() => setOrderBy("title")}>Title</button>
+            <button onClick={() => setOrderBy("rating")}>Rating</button>
+            {orderBy}
+          </div>
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard
+                key={smoothie.id}
+                smoothie={smoothie}
+                onDelete={handleDelete}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
